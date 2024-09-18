033fc541d1d3dac3d7be4d9a5cdaffcf33b38bae
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-09-18 21:14:19 -0230

    Completed Problem 1

--- Problem 1/Account.java
+++ Problem 1/Account.java
@@ -1,10 +1,17 @@
 public class Account {
+
     //Attributes
     private String id ;
     private String name ;
     private int balance = 0;
-
+    
     //Parameterized Constructor
+    public Account(String id, String name) 
+    {
+        this.id = id;
+        this.name = name;
+    }
+
     public Account(String id, String name, int balance)
     {
         this.id = id;
@@ -28,4 +35,40 @@ public class Account {
         return this.balance;
     }
 
+    public int credit(int amount)
+    {
+        return (this.balance + amount);
+    }
+
+    public int debit(int amount) 
+    {
+        if (amount <= this.balance) 
+        {
+            this.balance = this.balance - amount;
+        }
+        else 
+        {
+            System.out.println("Amount exceeded balance");
+        }
+        return this.balance;
+    }
+
+    public int transferTo(Account receivingAcc, int amount) 
+    {
+        if (amount <= this.balance) {
+            receivingAcc.balance += amount;
+            this.balance -= amount;
+        }
+        else {
+            System.out.println("Amount exceeded balance.");
+        }
+        return this.balance;
+    }
+
+    public String toString() 
+    {
+        String accountString = "Account [id=" + this.id + ",name=" + this.name + ",balance=" + this.balance;
+        System.out.println(accountString);
+        return accountString;
+    }
 }
--- Problem 1/TestAccount.java
+++ Problem 1/TestAccount.java
@@ -1,3 +1,24 @@
 public class TestAccount {
-    
+    public static void main(String[] args) {
+
+        //a
+        Account Acc1 = new Account("Acc1", "Jim", 5000);
+        Account Acc2 = new Account("Acc2", "Angus", 4000);
+
+
+        //b
+        System.out.println("Account 1 Balance:");
+        System.out.println(Acc1.getBalance());
+        System.out.println("Account 2 Balance:");
+        System.out.println(Acc2.getBalance());
+
+        //c
+        Acc1.transferTo(Acc2, 1000);
+
+        //d 
+        System.out.println("Account 1 Balance:");
+        System.out.println(Acc1.getBalance());
+        System.out.println("Account 2 Balance:");
+        System.out.println(Acc2.getBalance());
+    }
 }
