3e770357f6b887fd5dad07b45f0ddb6d6be2cf74
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-09-19 09:53:27 -0230

    Data.java complete

--- Problem 2/Date.java
+++ Problem 2/Date.java
@@ -8,8 +8,56 @@ public class Date {
     //Constructors
     public Date(int day, int month, int year) 
     {
+        this.day = day;
+        this.month = month;
+        this.year = year;
     }
 
     //Methods
+    public int getDay() 
+    {
+       return this.day;
+
+    }
+
+    public int getMonth()
+    {
+        return this.month;
+    }
+
+    public int getYear()
+    {
+        return this.year;
+    }
+
+    public void setDay(int day)
+    {
+        this.day = day;
+
+    }
+
+    public void setMonth(int month)
+    {
+        this.month = month;
+    }
     
+    public void setYear(int year)
+    {
+        this.year = year;
+    }
+
+    public void setDate(int day, int month, int year)
+    {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+
+    }
+
+    public String toString() 
+    {
+        String dateString = this.day + "/" + this.month + "/" + this.year;
+        System.out.println(dateString);
+        return dateString;
+    }
 }
