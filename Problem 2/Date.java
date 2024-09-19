b28005cc95ca4ae11be39765552c4e354161eab0
chelseaslade <153530277+chelseaslade@users.noreply.github.com>
2024-09-19 09:37:43 -0230

    Start of Problem 2

---
+++ Problem 2/Date.java
@@ -0,0 +1,15 @@
+public class Date {
+
+    //Attributes
+    private int day;
+    private int month;
+    private int year;
+
+    //Constructors
+    public Date(int day, int month, int year) 
+    {
+    }
+
+    //Methods
+    
+}
